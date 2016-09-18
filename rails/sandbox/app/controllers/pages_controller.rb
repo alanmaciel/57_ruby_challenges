c499313e9176f9f67f8ddb49359dc1cec56d51e9
Alan Maciel <alan.maciel.salcedo@gmail.com>
2016-09-18 15:45:48 -0500

    Added gui Challenge 2 in Rails using jQuery

---
+++ rails/sandbox/app/controllers/pages_controller.rb
@@ -0,0 +1,7 @@
+class PagesController < ApplicationController
+  def index
+  end
+
+  def example2
+  end
+end
--- rails/sandbox/config/routes.rb
+++ rails/sandbox/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get "/pages/challenge2", to: "pages#challenge2", as: "challenge2"
+  
+  resources "pages"
+  root 'pages#index'
 end
