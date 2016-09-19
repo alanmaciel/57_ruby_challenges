c2ce0b14ddfc23c1620bbb1b7dc1a182e58249b8
Alan Maciel <alan.maciel.salcedo@gmail.com>
2016-09-19 16:41:43 -0500

    Exercise 5 - Simple math prompt for 2 numbers and do a sum, difference, product and quotient.

---
+++ 05/simple_math.rb
@@ -0,0 +1,29 @@
+# 5- Simple Math 
+# Program that prompts for two numbers, print the sum, difference, product
+# and quotient of those numbers as following
+#
+# Output:
+# What is the first number? 10
+# What is the second number? 5
+# 10 + 5 = 15
+# 10 - 5 = 5
+# 10 * 5 = 50
+# 10 / 5 = 2 
+#
+
+loop do
+  puts "What is the first number? "
+  first_number = gets.chomp.to_i
+  puts "What is the second number? "
+  second_number = gets.chomp.to_i
+  sum = first_number + second_number
+  difference = first_number - second_number
+  product = first_number * second_number
+  quotient = first_number / second_number
+
+  puts "#{first_number} + #{second_number} = #{sum}"
+  puts "#{first_number} - #{second_number} = #{difference}"
+  puts "#{first_number} * #{second_number} = #{product}"
+  puts "#{first_number} / #{second_number} = #{quotient}"
+
+end
