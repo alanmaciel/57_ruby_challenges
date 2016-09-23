927253170706d0279ac38177f0a4e911fae14a2c
Alan Maciel <alan.maciel.salcedo@gmail.com>
2016-09-22 19:47:58 -0500

    Exercise 5 - Simple Math GUI version made with javascript

--- rails/sandbox/config/routes.rb
+++ rails/sandbox/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   get "/pages/challenge2", to: "pages#challenge2", as: "challenge2"
+  get "/pages/challenge5", to: "pages#challenge5", as: "challenge5"
   
   resources "pages"
   root 'pages#index'
